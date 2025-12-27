14161cece76c7f88f73db48b721cb17dd7853cb5
Christian Aspinwall <141345150+Caspinwall1@users.noreply.github.com>
2025-12-27 16:21:24 -0500

    Implement visitor counter API fetch
    Added fetch call to update visitor counter from API.

--- Front End/script.js
+++ Front End/script.js
@@ -1,1 +1,8 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+fetch("https://resume-visitor-api.azurewebsites.net/api/visitcounter")
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById("counter").textContent = data.count;
+  })
+  .catch(err => console.error("Visitor counter error:", err));
